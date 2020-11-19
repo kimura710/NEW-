af444e5fe85c202f093fbcbdde30e257e0d5c636
kimura <pankata79.th@gmail.com>
2020-11-20 06:31:26 +0900

    テスト

---
+++ src/test/java/com/example/demo/controllerTest/SecurityControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.Portfolio1Application;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = Portfolio1Application.class)
+public class SecurityControllerTest {
+
+	// Tomcatサーバへデプロイする事なくHttpリクエスト・レスポンスを扱うためのMockオブジェクト
+	@Autowired
+	private MockMvc mockMvc;
+	
+	
+	
+}
